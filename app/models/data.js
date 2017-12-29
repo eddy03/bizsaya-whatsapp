dfb207512c61a8327ae5d26a9c1493c6aae91318
Edi Abdul Rahman <eddy.rahman@thestar.com.my>
2017-12-29 13:16:35 +0800

    add page name

--- app/models/data.js
+++ app/models/data.js
@@ -18,6 +18,7 @@ module.exports = {
   saveData: data => {
     global.REDIS.set(data.id, JSON.stringify({
       phone: data.phone,
+      name: data.page_name,
       msg: _.isEmpty(data.message) ? '' : encodeURIComponent(data.message),
       hit: 0
     }))
